6076ce5a199c5d731f2572d7a05385d6ee199c83
bill-comer <bill.comer.github@beanfactory.co.uk>
2011-02-10 10:39:39 +0000

    code layout

--- test/unit/uk/co/beanfactory/grails/database/type/PostcodeTypeTests.groovy
+++ test/unit/uk/co/beanfactory/grails/database/type/PostcodeTypeTests.groovy
@@ -12,35 +12,37 @@ import com.mockrunner.mock.jdbc.MockResultSet
 
 class PostcodeTypeTests
 {
-  void testNullSafeGet() {
+  void testNullSafeGet()
+  {
     PostcodeType postcodeType = new PostcodeType()
-	
+
     MockResultSet resultSet = new MockResultSet("testMock")
-	
+
     //resultSet.setResultSetType(Types.VARCHAR)
     String[] names = "postcode"
-    
-	ArrayList<String> mockResultData = new ArrayList<String>()
+
+    ArrayList<String> mockResultData = new ArrayList<String>()
     mockResultData.add("BL7 0NG")
     resultSet.addColumn(names[0], mockResultData)
-	resultSet.next()
-    
+    resultSet.next()
+
     Object owner = null;
-    
-	//test method
+
+    //test method
     Postcode createdPostcode = postcodeType.nullSafeGet (resultSet, names, owner)
-	
-	assertEquals ("BL7", createdPostcode.getOutward())
-	assertEquals ("0NG", createdPostcode.getInward())
+
+    assertEquals ("BL7", createdPostcode.getOutward())
+    assertEquals ("0NG", createdPostcode.getInward())
   }
-  
-  void testNullSafeSet() {
-	  PostcodeType postcodeType = new PostcodeType()
-	  
-	  Postcode postcode = new Postcode(outward:"BL7", inward:"0NG")
-	  
-	  //test method
-	  postcodeType.nullSafeSet (statement, value, 1)
-	  
+
+  void testNullSafeSet()
+  {
+    PostcodeType postcodeType = new PostcodeType()
+
+    Postcode postcode = new Postcode(outward:"BL7", inward:"0NG")
+
+    //test method
+    postcodeType.nullSafeSet (statement, value, 1)
+
   }
 }
