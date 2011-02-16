565d199f49952acb2cb9d186496c7630747847c7
bill-comer <bill.comer.github@beanfactory.co.uk>
2011-02-16 15:40:50 +0000

    Add BootStrap for a test AssetGroup

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,10 @@
+import uk.co.assetmap.AssetGroup
+
+
 class BootStrap {
 
     def init = { servletContext ->
+      new AssetGroup(name:"bootstrapAssGrp", description:"a desc",created:new Date()).save()
     }
     def destroy = {
     }
